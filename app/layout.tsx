8636fcbc81cb46132cdba2a77c931b282370281a
James Walsh <jamesandersonwalsh@gmail.com>
2023-06-29 10:59:12 -0600

    Improve mobile responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,12 +19,21 @@ const mainStyles = container({
 const paperStyles = vstack({
   height: '100vh',
   bg: 'slate.900',
-  mx: '14rem',
+  xl: {
+    mx: '14rem',
+  },
+  md: {
+    mx: '8rem',
+  },
+  sm: {
+    mx: '2rem',
+  },
   paddingTop: '2rem',
 })
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body className={inter.className}>
         <div className={paperStyles}>
           <TopNavbar />
@@ -36,11 +45,17 @@ export default function RootLayout({ children }: PropsWithChildren) {
 }
 
 const navBarStyles = hstack({
+  hideBelow: 'sm',
   bg: 'slate.800',
   h: '2.5rem',
   px: '2rem',
   fontWeight: 'normal',
-  maxWidth: '30vw',
+  lg: {
+    maxWidth: '40vw',
+  },
+  md: {
+    maxWidth: '70vw',
+  },
   borderRadius: 'full',
   borderWidth: '1px',
   borderColor: 'slate.700',
@@ -53,6 +68,9 @@ const navItemStyles = css({
   alignItems: 'center',
   px: '0.5rem',
   borderRadius: 'md',
+  sm: {
+    fontSize: 'md',
+  },
 })
 function TopNavbar() {
   return (
