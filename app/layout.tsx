38059e8c42af6565cbe7de171c6f387777a8b5b5
James Walsh <jamesandersonwalsh@gmail.com>
2023-06-28 22:04:51 -0600

    Add basic hover states to right nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import { PropsWithChildren } from 'react'
 import { hstack } from 'styled-system/patterns'
+import { css } from 'styled-system/css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -31,23 +32,34 @@ const navBarStyles = hstack({
   fontWeight: 'normal',
 })
 const rightNavStyles = hstack({
-  gap: 8,
+  gap: 4,
+  height: '100%',
+})
+const rightNavItemStyles = css({
+  display: 'flex',
+  alignItems: 'center',
+  height: '3/4',
+  px: '0.5rem',
+  borderRadius: 'md',
+  _hover: {
+    bg: 'indigo.400',
+  },
 })
 function TopNavbar() {
   return (
     <nav className={navBarStyles}>
       <Link href="/">James Walsh</Link>
       <ul className={rightNavStyles}>
-        <li>
+        <li className={rightNavItemStyles}>
           <Link href="#">About</Link>
         </li>
-        <li>
+        <li className={rightNavItemStyles}>
           <Link href="#">Projects</Link>
         </li>
-        <li>
+        <li className={rightNavItemStyles}>
           <Link href="#">Blog</Link>
         </li>
-        <li>
+        <li className={rightNavItemStyles}>
           <Link href="#">Contact Me</Link>
         </li>
       </ul>
