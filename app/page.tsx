3372cf82b0ed3916d3124103fc6da7aa7d6c2543
James Walsh <jamesandersonwalsh@gmail.com>
2023-06-28 21:25:00 -0600

    Add container to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import { css } from "styled-system/css";
+import { container } from 'styled-system/patterns'
+
+const mainStyles = container({ textAlign: 'center', my: '4rem' })
 
 export default function Home() {
   return (
-    <main>
+    <main className={mainStyles}>
       <h1>Under construction. Please come back later.</h1>
     </main>
-  );
+  )
 }
