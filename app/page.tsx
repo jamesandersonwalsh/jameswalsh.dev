fc85c331bcfe6fb5adf51aa0e4d2f279678cc4b4
James Walsh <jamesandersonwalsh@gmail.com>
2023-06-29 06:48:06 -0600

    Add an avatar to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import { container } from 'styled-system/patterns'
+import { container, circle } from 'styled-system/patterns'
 import { css } from 'styled-system/css'
+import Image from 'next/image'
 
 const mainStyles = container()
 
@@ -11,6 +12,13 @@ export default function Home() {
     <div className={mainStyles}>
       <section className={nameSectionStyles}>
         <h1>Hi 👋🏻 I&apos;m James</h1>
+        <Image
+          src="/profile.jpg"
+          className={circle({ size: 360, border: '2px solid white' })}
+          width={360}
+          height={360}
+          alt="picture of James"
+        />
         <h2>Full Stack JavaScript Engineer & Experienced Web Enthusiast</h2>
       </section>
     </div>
