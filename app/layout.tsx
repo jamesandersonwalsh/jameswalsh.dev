5579c6808d69f0ca297de51103077343f1971b94
James Walsh <jamesandersonwalsh@gmail.com>
2023-06-29 08:21:04 -0600

    Implement paper UX

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './global.css'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import { PropsWithChildren } from 'react'
-import { hstack, container } from 'styled-system/patterns'
+import { hstack, vstack, container } from 'styled-system/patterns'
 import { css } from 'styled-system/css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,57 +13,62 @@ export const metadata = {
 }
 
 const mainStyles = container({
-  my: '8rem',
+  mx: '8rem',
+  py: '4rem',
+})
+const paperStyles = vstack({
+  height: '100vh',
+  bg: 'slate.800',
+  mx: '14rem',
+  paddingTop: '2rem',
 })
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <TopNavbar />
-        <main className={mainStyles}>{children}</main>
+        <div className={paperStyles}>
+          <TopNavbar />
+          <main className={mainStyles}>{children}</main>
+        </div>
       </body>
     </html>
   )
 }
 
 const navBarStyles = hstack({
-  justifyContent: 'space-between',
-  bg: 'indigo.500',
-  h: '4rem',
+  bg: 'indigo.800',
+  h: '2.5rem',
   px: '2rem',
-  fontSize: 'xl',
   fontWeight: 'normal',
+  maxWidth: '30vw',
+  borderRadius: 'full',
+  borderWidth: '1px',
+  borderColor: 'indigo.900',
 })
-const rightNavStyles = hstack({
+const navStyles = hstack({
   gap: 4,
-  height: '100%',
 })
-const rightNavItemStyles = css({
+const navItemStyles = css({
   display: 'flex',
   alignItems: 'center',
-  height: '3/4',
   px: '0.5rem',
   borderRadius: 'md',
-  _hover: {
-    bg: 'indigo.400',
-  },
 })
 function TopNavbar() {
   return (
     <nav className={navBarStyles}>
-      <Link href="/">James Walsh</Link>
-      <ul className={rightNavStyles}>
-        <li className={rightNavItemStyles}>
+      <ul className={navStyles}>
+        <li className={navItemStyles}>
           <Link href="#">About</Link>
         </li>
-        <li className={rightNavItemStyles}>
-          <Link href="#">Projects</Link>
-        </li>
-        <li className={rightNavItemStyles}>
+        <li className={navItemStyles}>
           <Link href="#">Blog</Link>
         </li>
-        <li className={rightNavItemStyles}>
-          <Link href="#">Contact Me</Link>
+        <li className={navItemStyles}>
+          <Link href="#">Projects</Link>
+        </li>
+        <li className={navItemStyles}>
+          <Link href="#">Stack</Link>
         </li>
       </ul>
     </nav>
