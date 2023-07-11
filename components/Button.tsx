764514c01839f959c7d8a0ee8d056c4d1c059420
James Walsh <jamesandersonwalsh@gmail.com>
2023-07-10 21:35:08 -0600

    Create multi-variant button with cva

---
+++ components/Button.tsx
@@ -0,0 +1,58 @@
+import { PropsWithChildren } from 'react'
+import { cva } from 'styled-system/css'
+
+const button = cva({
+  base: {
+    display: 'flex',
+    flexDirection: 'row',
+    borderRadius: 'lg',
+    boxShadow: 'lg',
+    width: '100%',
+    fontWeight: 'semibold',
+    _hover: {
+      cursor: 'pointer',
+    },
+  },
+  variants: {
+    visual: {
+      primary: {
+        bg: 'slate.800',
+        color: 'slate.300',
+        _hover: {
+          bg: 'slate.700',
+          color: 'slate.100',
+        },
+      },
+      outline: {
+        borderWidth: '1px',
+        borderColor: 'slate.700',
+        bg: 'transparent',
+      },
+    },
+    size: {
+      sm: { padding: '0.25rem', fontSize: 'sm' },
+      md: { padding: '0.7rem', fontSize: 'md' },
+      lg: { padding: '1rem', fontSize: '24px' },
+    },
+  },
+})
+
+type ButtonProps = {
+  variant?: 'primary' | 'outline'
+  as?: 'a' | 'button'
+  size?: 'sm' | 'md' | 'lg'
+  href?: string
+} & PropsWithChildren
+
+export function Button({
+  variant = 'primary',
+  as = 'button',
+  size = 'md',
+  ...rest
+}: ButtonProps) {
+  if (as === 'button') {
+    return <button className={button({ visual: variant, size })} {...rest} />
+  } else {
+    return <a className={button({ visual: variant, size })} {...rest} />
+  }
+}
