bdf75f98d6ae8d94ab2277a295275ba7be31770b
James Walsh <jamesandersonwalsh@gmail.com>
2023-07-15 15:35:42 -0600

    Create shared List componentry

--- components/List.tsx
+++ components/List.tsx
@@ -1,13 +1,40 @@
-import { PropsWithChildren } from 'react'
+import { DetailedHTMLProps, LiHTMLAttributes, PropsWithChildren } from 'react'
+import { vstack, hstack } from 'styled-system/patterns'
 
-export function OrderedList({ children }: PropsWithChildren) {
-  return <ol>{children}</ol>
+const orderedList = vstack({
+  listStyle: 'auto',
+  gap: 6,
+})
+export function OrderedList({ children, ...rest }: PropsWithChildren) {
+  return (
+    <ol className={orderedList} {...rest}>
+      {children}
+    </ol>
+  )
 }
 
-export function UnorderedList({ children }: PropsWithChildren) {
-  return <ul>{children}</ul>
+const unorderedList = vstack({
+  gap: 6,
+})
+export function UnorderedList({ children, ...rest }: PropsWithChildren) {
+  return (
+    <ul className={unorderedList} {...rest}>
+      {children}
+    </ul>
+  )
 }
 
-export function ListItem({ children }: PropsWithChildren) {
-  return <ul>{children}</ul>
+const listItem = hstack({
+  width: '100%',
+})
+
+type ListItemProps = DetailedHTMLProps<
+  LiHTMLAttributes<HTMLLIElement>,
+  HTMLLIElement
+>
+export function ListItem(props: ListItemProps) {
+  return <li className={listItem} {...props} />
 }
+
+OrderedList.ListItem = listItem
+UnorderedList.ListItem = ListItem
