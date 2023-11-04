0539015e233875edfeecb7e7b4e374e1fb572483
James Walsh <jamesandersonwalsh@gmail.com>
2023-11-03 20:29:53 -0600

    Remove panda from posts page

---
+++ app/posts/read-more.tsx
@@ -0,0 +1,9 @@
+import { ChevronRight } from 'lucide-react'
+
+export function ReadMore() {
+  return (
+    <span className="align-center flex flex-row items-center gap-1 text-base font-semibold text-blue-500 hover:to-blue-400">
+      Read More <ChevronRight width={20} height={20} />
+    </span>
+  )
+}
