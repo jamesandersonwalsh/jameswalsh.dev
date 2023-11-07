c605578c40d13a2d7c0d8368ea314d3aaa716e12
James Walsh <jamesandersonwalsh@gmail.com>
2023-11-06 20:43:50 -0700

    fix read-more flex positioning

--- app/posts/read-more.tsx
+++ app/posts/read-more.tsx
@@ -2,7 +2,7 @@ import { ChevronRight } from 'lucide-react'
 
 export function ReadMore() {
   return (
-    <span className="align-center flex flex-row items-center gap-1 text-base font-semibold text-blue-500 hover:to-blue-400">
+    <span className="align-center flex flex-row items-end gap-1 text-base font-semibold text-blue-500 hover:to-blue-400">
       Read More <ChevronRight width={20} height={20} />
     </span>
   )
