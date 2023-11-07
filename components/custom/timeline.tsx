63d21690c4a20091e36d4352af0dd7a1d40190f6
James Walsh <jamesandersonwalsh@gmail.com>
2023-11-06 20:20:21 -0700

    Remove the last of pandacss

--- components/custom/timeline.tsx
+++ components/custom/timeline.tsx
@@ -1,64 +1,17 @@
 import { PropsWithChildren } from 'react'
-import { grid, gridItem, stack } from 'styled-system/patterns'
 
-export function Timeline({ children }: PropsWithChildren) {
-  return (
-    <ol
-      className={stack({
-        width: '100%',
-        gap: 12,
-        px: '1.5rem',
-        mt: '1.5rem',
-        borderInlineStart: '1px solid',
-        borderInlineStartColor: 'slate.500',
-      })}
-    >
-      {children}
-    </ol>
-  )
+export function Timeline(props: PropsWithChildren) {
+  return <ol className="mt-6 flex w-full flex-col gap-12 px-2" {...props} />
 }
 
-export function TimelineItem({ children }: PropsWithChildren) {
-  return (
-    <li
-      className={grid({
-        columns: {
-          md: 4,
-          sm: 1,
-        },
-      })}
-    >
-      {children}
-    </li>
-  )
+export function TimelineItem(props: PropsWithChildren) {
+  return <li className="grid grid-cols-1 md:grid-cols-4" {...props} />
 }
 
-export function TimelineLeftElement({ children }: PropsWithChildren) {
-  return (
-    <div
-      className={gridItem({
-        colSpan: 1,
-        mt: '1rem',
-      })}
-    >
-      {children}
-    </div>
-  )
+export function TimelineLeftElement(props: PropsWithChildren) {
+  return <div className="col-span-1 mt-4" {...props} />
 }
 
-export function TimelineRightElement({ children }: PropsWithChildren) {
-  return (
-    <div
-      className={gridItem({
-        display: 'flex',
-        justifyContent: 'center',
-        colSpan: {
-          md: 3,
-          sm: 1,
-        },
-      })}
-    >
-      {children}
-    </div>
-  )
+export function TimelineRightElement(props: PropsWithChildren) {
+  return <div className="col-span-3" {...props} />
 }
