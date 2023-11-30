e940177c2756eb44540513280fb7d61e03ebe3e0
James Walsh <jamesandersonwalsh@gmail.com>
2023-11-30 11:29:43 -0800

    Remove size prop from time component

--- components/time.tsx
+++ components/time.tsx
@@ -1,28 +1,17 @@
-import { cva } from 'class-variance-authority'
 import formatDate from 'date-fns/format'
 import { PropsWithChildren, TimeHTMLAttributes } from 'react'
-
-const timeVariants = cva('', {
-  variants: {
-    size: {
-      sm: 'text-sm',
-      md: 'text-md',
-    },
-  },
-})
 
 type TimeFormatProps = {
   dateTime: string
   format?: 'MMM dd, yyyy'
-  size?: 'sm' | 'md'
 } & TimeHTMLAttributes<HTMLTimeElement> &
   PropsWithChildren
 
-export function Time({ dateTime, format = 'MMM dd, yyyy', size = 'md', ...rest }: TimeFormatProps) {
+export function Time({ dateTime, format = 'MMM dd, yyyy', ...rest }: TimeFormatProps) {
   const date = new Date(dateTime)
 
   return (
-    <time className={timeVariants({ size })} dateTime={dateTime} {...rest}>
+    <time className="text-sm md:text-lg" dateTime={dateTime} {...rest}>
       {formatDate(date, format)}
     </time>
   )
