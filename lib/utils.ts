73b30f74f84386f1855461e0957ffa255e89f12e
James Walsh <jamesandersonwalsh@gmail.com>
2024-02-06 21:56:21 -0700

    Instantiate vitest

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,6 +6,10 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function calculateTimeToRead(text: string): number {
+  if (!text.length) {
+    return 0
+  }
+
   const wordsPerMinute = 265
   const numberOfWords = text.trim().split(/\s+/).length
 
