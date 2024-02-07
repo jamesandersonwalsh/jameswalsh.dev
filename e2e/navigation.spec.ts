f47971d122bebd11415da444a44792a63d80f199
James Walsh <jamesandersonwalsh@gmail.com>
2024-02-07 13:48:22 -0700

    Include previous cypress footer tests

--- e2e/navigation.spec.ts
+++ e2e/navigation.spec.ts
@@ -5,60 +5,96 @@ test.beforeEach(async ({ page }, testInfo) => {
   await page.goto('/')
 })
 
-test('navigates to /posts', async ({ page, isMobile }) => {
-  if (isMobile) {
-    await page.locator('data-testid=mobile-menu-trigger').click()
-    await page.getByRole('list').getByTestId('blog-nav-item').click()
-  } else {
-    await page.locator('nav').getByTestId('blog-nav-item').click()
-  }
+test.describe('when navigating the top nav bar', () => {
+  test('navigates to /posts', async ({ page, isMobile }) => {
+    if (isMobile) {
+      await page.locator('data-testid=mobile-menu-trigger').click()
+      await page.getByRole('list').getByTestId('blog-nav-item').click()
+    } else {
+      await page.locator('nav').getByTestId('blog-nav-item').click()
+    }
 
-  await expect(page).toHaveURL('/posts')
-  await expect(page).toHaveTitle('Articles - James Walsh')
-  await expect(page.getByText('Latest Blog Posts')).toBeVisible()
+    await expect(page).toHaveURL('/posts')
+    await expect(page).toHaveTitle('Articles - James Walsh')
+    await expect(page.getByText('Latest Blog Posts')).toBeVisible()
+  })
+
+  test('navigates to /about', async ({ page, isMobile }) => {
+    if (isMobile) {
+      await page.locator('data-testid=mobile-menu-trigger').click()
+      await page.getByRole('list').getByTestId('about-nav-item').click()
+    } else {
+      await page.locator('nav').getByTestId('about-nav-item').click()
+    }
+
+    await expect(page).toHaveURL('/about')
+    await expect(page).toHaveTitle('About - James Walsh')
+    await expect(page.getByText(`I'm James`)).toBeVisible()
+  })
+
+  test('navigates to /stack', async ({ page, isMobile }) => {
+    if (isMobile) {
+      await page.locator('data-testid=mobile-menu-trigger').click()
+      await page.getByRole('list').getByTestId('stack-nav-item').click()
+    } else {
+      await page.locator('nav').getByTestId('stack-nav-item').click()
+    }
+
+    await expect(page).toHaveURL('/stack')
+    await expect(page).toHaveTitle('Stack - James Walsh')
+    await expect(page.getByText(`Technology Tools I Recommend & Use`)).toBeVisible()
+  })
+
+  test('navigates to /projects', async ({ page, isMobile }) => {
+    if (isMobile) {
+      await page.locator('data-testid=mobile-menu-trigger').click()
+      await page.getByRole('list').getByTestId('portfolio-nav-item').click()
+    } else {
+      await page.locator('nav').getByTestId('portfolio-nav-item').click()
+    }
+
+    await expect(page).toHaveURL('/projects')
+    await expect(page).toHaveTitle('Projects - James Walsh')
+    await expect(page.getByText(`Things I've Helped Build That`)).toBeVisible()
+  })
+
+  test('Makes /rss.xml available on desktop', async ({ page, isMobile }) => {
+    if (isMobile) return
+
+    await page.locator('data-testid=rss-feed-nav-item').click()
+  })
 })
 
-test('navigates to /about', async ({ page, isMobile }) => {
-  if (isMobile) {
-    await page.locator('data-testid=mobile-menu-trigger').click()
-    await page.getByRole('list').getByTestId('about-nav-item').click()
-  } else {
-    await page.locator('nav').getByTestId('about-nav-item').click()
-  }
+test.describe('when navigating the footer', () => {
+  test('navigates to /posts', async ({ page }) => {
+    await page.getByTestId('blog-footer-nav-item').click()
 
-  await expect(page).toHaveURL('/about')
-  await expect(page).toHaveTitle('About - James Walsh')
-  await expect(page.getByText(`I'm James`)).toBeVisible()
-})
+    await expect(page).toHaveURL('/posts')
+    await expect(page).toHaveTitle('Articles - James Walsh')
+    await expect(page.getByText('Latest Blog Posts')).toBeVisible()
+  })
 
-test('navigates to /stack', async ({ page, isMobile }) => {
-  if (isMobile) {
-    await page.locator('data-testid=mobile-menu-trigger').click()
-    await page.getByRole('list').getByTestId('stack-nav-item').click()
-  } else {
-    await page.locator('nav').getByTestId('stack-nav-item').click()
-  }
+  test('navigates to /about', async ({ page }) => {
+    await page.getByTestId('about-footer-nav-item').click()
 
-  await expect(page).toHaveURL('/stack')
-  await expect(page).toHaveTitle('Stack - James Walsh')
-  await expect(page.getByText(`Technology Tools I Recommend & Use`)).toBeVisible()
-})
+    await expect(page).toHaveURL('/about')
+    await expect(page).toHaveTitle('About - James Walsh')
+    await expect(page.getByText(`I'm James`)).toBeVisible()
+  })
 
-test('navigates to /portfolio', async ({ page, isMobile }) => {
-  if (isMobile) {
-    await page.locator('data-testid=mobile-menu-trigger').click()
-    await page.getByRole('list').getByTestId('portfolio-nav-item').click()
-  } else {
-    await page.locator('nav').getByTestId('portfolio-nav-item').click()
-  }
+  test('navigates to /stack', async ({ page }) => {
+    await page.getByTestId('stack-footer-nav-item').click()
 
-  await expect(page).toHaveURL('/projects')
-  await expect(page).toHaveTitle('Projects - James Walsh')
-  await expect(page.getByText(`Things I've Helped Build That`)).toBeVisible()
-})
+    await expect(page).toHaveURL('/stack')
+    await expect(page).toHaveTitle('Stack - James Walsh')
+    await expect(page.getByText(`Technology Tools I Recommend & Use`)).toBeVisible()
+  })
 
-test('Makes /rss.xml available on desktop', async ({ page, isMobile }) => {
-  if (isMobile) return
+  test('navigates to /projects', async ({ page }) => {
+    await page.getByTestId('portfolio-footer-nav-item').click()
 
-  await page.locator('data-testid=rss-feed-nav-item').click()
+    await expect(page).toHaveURL('/projects')
+    await expect(page).toHaveTitle('Projects - James Walsh')
+    await expect(page.getByText(`Things I've Helped Build That`)).toBeVisible()
+  })
 })
