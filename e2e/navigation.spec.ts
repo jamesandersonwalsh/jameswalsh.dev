b7c0195902bb76b348634af828aa357314a9b03a
James Walsh <jamesandersonwalsh@gmail.com>
2024-02-06 23:04:23 -0700

    Update playwright.config.ts with proper values

--- e2e/navigation.spec.ts
+++ e2e/navigation.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test.beforeEach(async ({ page }, testInfo) => {
   console.log(`Running ${testInfo.title}`)
-  await page.goto('./')
+  await page.goto('/')
 })
 
 test('navigates to /posts', async ({ page }) => {
--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
-    baseURL: 'http://127.0.0.1:3001',
+    baseURL: 'http://127.0.0.1:3000',
   },
 
   projects: [
@@ -60,8 +60,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
-    url: 'http://127.0.0.1:3001',
+    command: 'pnpm build && pnpm start',
+    url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
   },
 })
