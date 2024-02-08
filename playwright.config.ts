d253efa6e4b3bcc3062e8b28ac5875a26ea21ccf
James Walsh <jamesandersonwalsh@gmail.com>
2024-02-07 19:11:57 -0700

    Use IP address rather than localhost in playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test'
 
 const PORT = process.env.PORT || 3000
-const baseURL = `http://localhost:${PORT}`
+const baseURL = `http://127.0.0.1:${PORT}`
 
 export default defineConfig({
   testDir: './e2e',
