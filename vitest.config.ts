ac64835ac08b076de83a6e032c391dcd5a05a144
James Walsh <jamesandersonwalsh@gmail.com>
2024-02-07 22:12:22 -0700

    Remove straggling contentlayer code

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'happy-dom',
-    exclude: ['e2e/**', 'node_modules/**', '.next/**', '.contentlayer/**'],
+    exclude: ['e2e/**', 'node_modules/**', '.next/**'],
   },
 })
