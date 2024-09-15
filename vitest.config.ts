06379f8724a3066036d9410ae7f84c0a274191e0
James Walsh <jamesandersonwalsh@gmail.com>
2024-09-14 23:45:29 -0600

    Enable coverage reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,21 @@
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { defineConfig } from 'vitest/config'
+import { defineConfig, coverageConfigDefaults } from 'vitest/config'
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   test: {
     globals: true,
+    coverage: {
+      provider: 'istanbul',
+      exclude: [
+        './components/ui/**',
+        './app/posthog.ts',
+        './app/PostHogPageView.tsx',
+        '*.config.*',
+        ...coverageConfigDefaults.exclude,
+      ],
+    },
     setupFiles: './vitest-setup.ts',
     environment: 'happy-dom',
     exclude: ['e2e/**', 'node_modules/**', '.next/**'],
