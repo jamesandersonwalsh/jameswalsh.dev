52237948eba1c68d1bf12ee87a65b1140bc1056d
James Walsh <jamesandersonwalsh@gmail.com>
2024-10-13 15:26:37 -0600

    Host all images, remove external src

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+    remotePatterns: [],
   },
   async rewrites() {
     return [
