68f85431e4ce2ead4e1ac262d2543f4f43c41f6e
James Walsh <jamesandersonwalsh@gmail.com>
2024-10-13 16:22:52 -0600

    fix final cspell problems

--- app/about/about-me.tsx
+++ app/about/about-me.tsx
@@ -26,10 +26,10 @@ export default function AboutMe() {
         <AccordionContent className="my-4">
           Fast forward to summer of 2014, I was working full-time while taking a semester off from my business
           management major. Spring Semester had left me feeling like I was approaching an intersection in life. I knew
-          my major wasn&apos;t a great fit, but I wasn&apos;t sure what was next. One night in early June 2014, I was
+          my major {`wasn't`} a great fit, but I {`didn't`} know what was next. One night in early June 2014, I was
           driving home bursting with excitement because for the first time ever I was going to boot up my new{' '}
           <b>Gaming PC</b>. As I thought about pressing the power button on my new gaming rig, a thought popped into my
-          head that I couldn&apos;t get rid of. Instead of playing games tonight, What if I sat down at the keyboard &
+          head that I {`couldn't`} get rid of. Instead of playing games tonight, What if I sat down at the keyboard &
           just tried to build something... Earlier that day, one of my teammates had mentioned that they were
           transitioning onto the software development team. & hearing that made something spark in my brain. Rather than
           rushing home every night to tinker with computers, I wanted to understand how they worked. I wanted to see if
