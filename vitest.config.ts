32aebae7f89dc061945a04796d4e193fe7f9022d
James Walsh <jamesandersonwalsh@gmail.com>
2024-10-21 22:42:28 -0600

    Increase coverage requirements

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,10 +10,10 @@ export default defineConfig({
       provider: 'istanbul',
       reporter: ['text', 'json-summary', 'json'],
       thresholds: {
-        statements: 65,
-        branches: 60,
-        functions: 65,
-        lines: 65,
+        statements: 80,
+        branches: 70,
+        functions: 80,
+        lines: 80,
       },
       exclude: [
         'components/ui/**', // ignore shadcn directory
