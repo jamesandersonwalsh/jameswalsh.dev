635257eb9f6348133dacefd66d7fe465300b3e2f
James Walsh <jamesandersonwalsh@gmail.com>
2024-10-21 20:31:14 -0600

    Unskip rss.xml tests

--- app/rss.xml/tests/route.test.ts
+++ app/rss.xml/tests/route.test.ts
@@ -9,7 +9,7 @@ vi.mock('@/app/posts/actions', () => ({
   fetchPublishedPosts: vi.fn().mockResolvedValue([]),
 }))
 
-describe.skip('rss.xml', () => {
+describe('rss.xml', () => {
   describe('GET', () => {
     const mockDateTime = new Date(2024, 10, 31)
 
