7c4b1924ed781a9740cbf88bf2179d876158a74c
James Walsh <jamesandersonwalsh@gmail.com>
2024-10-21 22:29:41 -0600

    Add unit tests to PostPage

---
+++ app/posts/[slug]/tests/page.test.tsx
@@ -0,0 +1,71 @@
+import { render, screen } from '@testing-library/react'
+import { PropsWithChildren } from 'react'
+
+import * as slugPageActions from '../actions'
+import PostPage from '../page'
+
+import { getMockPost } from '@/test/mocks/post'
+
+vi.mock('../actions', () => ({
+  fetchPostBySlug: vi.fn(),
+  fetchPreviousPost: vi.fn(() => Promise.resolve(undefined)),
+}))
+// vi.doMock('../actions')
+vi.mock('../mdx-content', () => ({
+  __esModule: true,
+  default: vi.fn(({ children }: PropsWithChildren) => <div>{children}</div>),
+}))
+describe('posts/[slug]/page', () => {
+  const mockSlug = 'my-slug'
+  const tags = ['typescript', 'javascript', 'nextjs', 'ssr', 'ssg']
+  const mockPost = getMockPost({ slug: mockSlug, title: 'How to win friends & influence people', tags })
+
+  const mockDateTime = new Date(Date.UTC(2024, 9, 31, 0, 0, 0)) // happy halloween 🎃
+
+  beforeAll(() => {
+    vi.useFakeTimers()
+    vi.setSystemTime(mockDateTime)
+  })
+
+  it('renders H1 for blog post', async () => {
+    vi.mocked(slugPageActions.fetchPostBySlug).mockResolvedValue(mockPost)
+
+    render(await PostPage({ params: { slug: mockSlug } }))
+
+    expect(screen.getByRole('heading', { name: mockPost.title })).toBeInTheDocument()
+  })
+
+  it('renders the blog thumbnail image', async () => {
+    vi.mocked(slugPageActions.fetchPostBySlug).mockResolvedValue(mockPost)
+
+    render(await PostPage({ params: { slug: mockSlug } }))
+    const articleImage = screen.getByAltText('Article cover image')
+    const encodedThumbnailSrc = encodeURIComponent(mockPost.thumbnail)
+
+    expect(articleImage).toBeInTheDocument()
+    expect(articleImage.getAttribute('src')).toContain(encodedThumbnailSrc)
+  })
+
+  it('renders all the blog tags', async () => {
+    vi.mocked(slugPageActions.fetchPostBySlug).mockResolvedValue(mockPost)
+
+    render(await PostPage({ params: { slug: mockSlug } }))
+
+    tags.forEach((tag) => {
+      expect(screen.getByText(`#${tag}`)).toBeInTheDocument()
+    })
+  })
+
+  it('renders blog time information', async () => {
+    vi.mocked(slugPageActions.fetchPostBySlug).mockResolvedValue(mockPost)
+
+    render(await PostPage({ params: { slug: mockSlug } }))
+
+    expect(screen.getByText('Sep 13, 2024')).toHaveAttribute('datetime', '2024-09-14')
+    expect(screen.getByText(/1 min read/i)).toBeInTheDocument()
+    expect(screen.getByText(/about 2 months ago/i)).toBeInTheDocument()
+  })
+
+  it.todo('renders a link to all posts')
+  it.todo('renders a link to the previous blog post')
+})
